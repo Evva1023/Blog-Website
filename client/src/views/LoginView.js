55dcad43ac7d7ad8f9adc4c016fedc965f6008c4
Evva1023 <evoosha@gmail.com>
2022-11-12 22:22:27 +0100

    Styled LoginView and RegisterView

--- client/src/views/LoginView.js
+++ client/src/views/LoginView.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginView = () => {
     return (
         <div className="login-container">
             <section className="login flex">
                 <h1>Log In</h1>
-                <form className="login-form">
-                    <input className="login-form-input" type="text" placeholder="Username" />
-                    <input className="login-form-input" type="password" placeholder="Password" />
-                    <button className="login-form-btn">Log In</button>
+                <form className="form">
+                    <input className="form-input" type="text" placeholder="Username" />
+                    <input className="form-input" type="password" placeholder="Password" />
+                    <button className="form-btn">Log In</button>
+                    <span>You don't have an account yet? <Link to="/register">Register Now</Link></span>
                 </form>
             </section>
         </div>
--- client/src/views/RegisterView.js
+++ client/src/views/RegisterView.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterView = () => {
     return (
-        <>
-        <h1>Register</h1>
-        <a href="/">Back to homepage</a>
-        </>
+        <div className="login-container">
+            <section className="login flex">
+                <h1>Register</h1>
+                <form className="form">
+                    <input className="form-input" type="text" placeholder="Username" required />
+                    <input className="form-input" type="email" placeholder="Email" required />
+                    <input className="form-input" type="password" placeholder="Password" required />
+                    <input className="form-input" type="password" placeholder="Confirm password" required />
+                    <button className="form-btn">Register</button>
+                    <span>Already have an account? <Link to="/login">Log In</Link></span>
+                </form>
+            </section>
+        </div>
     );
 };
