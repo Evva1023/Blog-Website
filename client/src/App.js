2f4d926b5ad27e00c04d8d6cbdd255df28c915b0
Evva1023 <evoosha@gmail.com>
2022-11-12 19:16:00 +0100

    Added BrowserRouter and routes, built navigation in HomeView component, and initially  styled links

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeView } from "./views/HomeView";
+import { LoginView } from "./views/LoginView";
+import { RegisterView } from "./views/RegisterView";
+import { SinglePostView } from "./views/SinglePostView";
+import { WritePostView } from "./views/WritePostView";
 import "./style.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeView />
+  },
+  {
+    path: "/login",
+    element: <LoginView />
+  },
+  {
+    path: "/register",
+    element: <RegisterView />
+  },
+  {
+    path: "/post/:id",
+    element: <SinglePostView />
+  },
+  {
+    path: "/write",
+    element: <WritePostView />
+  }
+]);
 
 export function App() {
   return (
-    <h1>Hello from React</h1>
+    <RouterProvider router={router} />
   );
 }
--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const HomeView = () => {
     return (
+        <>
+        <nav>
+            <Link className="nav-link" to="/login">Log In</Link>
+            <Link className="nav-link" to="/register">Register</Link>
+            <Link className="nav-link" to="/write">Write Post</Link>
+        </nav>
         <h1>Home Page</h1>
+        </>
     );
 };
