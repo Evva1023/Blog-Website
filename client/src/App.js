1c9d3235f5e1ebec003059a6294192e57e918be3
Evva1023 <evoosha@gmail.com>
2022-11-12 19:50:28 +0100

    Created Layout using Outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,53 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { HomeView } from "./views/HomeView";
 import { LoginView } from "./views/LoginView";
 import { RegisterView } from "./views/RegisterView";
 import { SinglePostView } from "./views/SinglePostView";
 import { WritePostView } from "./views/WritePostView";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+
 import "./style.css";
+
+const Layout = () => {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomeView />
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomeView />
+      },
+      {
+        path: "/post/:id",
+        element: <SinglePostView />
+      },
+      {
+        path: "/write",
+        element: <WritePostView />
+      }
+    ]
   },
   {
     path: "/login",
-    element: <LoginView />
+        element: <LoginView />
   },
   {
     path: "/register",
-    element: <RegisterView />
-  },
-  {
-    path: "/post/:id",
-    element: <SinglePostView />
-  },
-  {
-    path: "/write",
-    element: <WritePostView />
+        element: <RegisterView />
   }
+
 ]);
 
 export function App() {
--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { Header } from "../components/Header/Header";
-import { Footer } from "../components/Footer/Footer";
 
 export const HomeView = () => {
     return (
-        <>
-        <Header />
         <h1>Home Page</h1>
-        <Footer />
-        </>
     );
 };
